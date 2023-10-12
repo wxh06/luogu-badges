1ef23d25df8887c1008291570161863350256d9d
汪心禾 <wangxinhe06@gmail.com>
2023-10-12 16:14:37 +0800

    Fix unknown CCF level

--- src/app/badge/user/[uid]/ccf-level/route.ts
+++ src/app/badge/user/[uid]/ccf-level/route.ts
@@ -4,7 +4,8 @@ export const GET = userBadge(({ ccfLevel }) => {
   let color: string;
   if (ccfLevel >= 8) color = "#ffc116";
   else if (ccfLevel >= 6) color = "#3498db";
-  else color = "#52c41a";
+  else if (ccfLevel) color = "#52c41a";
+  else color = "inactive";
 
-  return { label: "CCF 等级", message: ccfLevel.toString(), color };
+  return { label: "CCF 等级", message: (ccfLevel || "未知").toString(), color };
 });
