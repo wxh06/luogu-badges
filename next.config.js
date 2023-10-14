9673502f1a75a2b4d90d821b291d914c45db3b97
汪心禾 <wangxinhe06@gmail.com>
2023-10-14 20:19:05 +0800

    SVG Logo proxy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import("next").NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    dangerouslyAllowSVG: true,
+    contentDispositionType: "attachment",
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lglg.top",
+        port: "",
+        pathname: "/piterator-x-exlg.svg",
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig;
