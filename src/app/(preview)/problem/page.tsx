8e9ee03cc84f9f707953eaaca5a2aab1eb3b0d0b
汪心禾 <wangxinhe06@gmail.com>
2023-10-21 23:27:31 +0800

    Update page titles

--- src/app/(preview)/problem/page.tsx
+++ src/app/(preview)/problem/page.tsx
@@ -1,6 +1,9 @@
+import type { Metadata } from "next";
 import ProblemBadges from "./problem-badges";
 
 const { DEFAULT_PROBLEM } = process.env;
+
+export const metadata: Metadata = { title: "题目徽章 - 洛谷徽章" };
 
 export default function Page() {
   return <ProblemBadges defaultProblem={DEFAULT_PROBLEM!} />;
--- src/app/(preview)/user/page.tsx
+++ src/app/(preview)/user/page.tsx
@@ -3,7 +3,7 @@ import UserBadges from "./user-badges";
 
 const { DEFAULT_USER } = process.env;
 
-export const metadata: Metadata = { title: "洛谷用户徽章" };
+export const metadata: Metadata = { title: "用户徽章 - 洛谷徽章" };
 
 export default function Page() {
   return <UserBadges defaultUser={DEFAULT_USER!} />;
